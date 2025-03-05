f0ce089b68e2d468b743970190293c59bf9feb06
TL <thomas.lees@outlook.com>
2025-03-05 12:45:38 +0000

    Touch controls - Combining scaling and panning - NOT complete

--- client/src/scenes/map/controls.tsx
+++ client/src/scenes/map/controls.tsx
@@ -57,10 +57,19 @@ export default class Controls {
     }
 
     static touch(controls: ControlsType) {
-        let initialTouches: TouchList
+        let initialTouches: TouchEvent
         let M0: number[][]
         let IM: number[][] | null
         let totalInitialTouches: number
+
+        const resetTouches = (e: TouchEvent): void => {
+            if (!e.touches) return
+            debug(`resetTouches: 
+                old inital touches = ${totalInitialTouches}
+                new inital touches = ${e.touches.length}`)
+            totalInitialTouches = e.touches.length
+            initialTouches = e
+        }
 
         function touchStarted(e: TouchEvent) {
             if (!e.touches || e.touches.length > 2 || e.touches.length < 1)
@@ -75,11 +84,19 @@ export default class Controls {
                 !e.touches ||
                 e.touches.length > 2 ||
                 e.touches.length < 1 ||
-                !initialTouches ||
+                !initialTouches.touches ||
                 !totalInitialTouches
             ) {
                 return
             }
+            M0 = controls.view.affineTranslation
+            IM = inverseAffineMatrix(controls.view.affineTranslation)
+            if (!IM) return
+            debug(`IM:
+                ${IM[0]}
+                ${IM[1]}
+                ${IM[2]}`)
+
             let translationX: number = 0
             let translationY: number = 0
 
@@ -93,181 +110,209 @@ export default class Controls {
                 //  Single Finger Touch - Translate Controls
                 //
                 debug(`One Touch`)
-                translationX = e.touches[0].clientX - initialTouches[0].clientX
-                translationY = e.touches[0].clientY - initialTouches[0].clientY
+                translationX =
+                    e.touches[0].clientX - initialTouches.touches[0].clientX
+                translationY =
+                    e.touches[0].clientY - initialTouches.touches[0].clientY
+                translationX = translationX * IM[0][0] + IM[0][2]
+                translationY = translationY * IM[0][0] + IM[1][2]
+
+                let MT: number[][] = [
+                    [1, 0, translationX],
+                    [0, 1, translationY],
+                    [0, 0, 1],
+                ]
+
+                let M0: number[][] = multiplyMatrices(M0, MT)
 
                 debug(`1 finger translation: (${translationX},${translationY})`)
             }
-            //
-            //  Double Finger Touch - Translate Controls
-            //
             if (e.touches.length === 2) {
+                //
+                //  Double Finger Touch - Translate Controls
+                //
                 debug(`Two Touch`)
                 translationX =
-                    midpoint(e.touches)[0] - midpoint(initialTouches)[0]
+                    midpoint(e.touches[0].clientX, e.touches[1].clientX) -
+                    midpoint(
+                        initialTouches.touches[0].clientX,
+                        initialTouches.touches[1].clientX
+                    )
                 translationY =
-                    midpoint(e.touches)[1] - midpoint(initialTouches)[1]
+                    midpoint(e.touches[0].clientY, e.touches[1].clientY) -
+                    midpoint(
+                        initialTouches.touches[0].clientY,
+                        initialTouches.touches[1].clientY
+                    )
             }
-            IM = inverseAffineMatrix(controls.view.affineTranslation)
-            if (!IM) return
-            debug(`IM:
-                ${IM[0]}
-                ${IM[1]}
-                ${IM[2]}`)
 
-            // controls.view.scale = distance(e.touches) / distance(initialTouches)
+            if (e.touches.length == 2) {
+                controls.view.scale =
+                    distance(
+                        [e.touches[0].clientX, e.touches[0].clientY],
+                        [e.touches[1].clientX, e.touches[1].clientY]
+                    ) /
+                    distance(
+                        [
+                            initialTouches.touches[0].clientX,
+                            initialTouches.touches[0].clientY,
+                        ],
+                        [
+                            initialTouches.touches[1].clientX,
+                            initialTouches.touches[1].clientY,
+                        ]
+                    )
 
-            // translationX =
-            //     midpoint(initialTouches)[0] +
-            //     (midpoint(initialTouches)[0] - midpoint(e.touches)[0])
+                let scaleTranslationX = 0
+                let scaleTranslationY = 0
 
-            // translationY =
-            //     midpoint(initialTouches)[1] +
-            //     (midpoint(initialTouches)[1] - midpoint(e.touches)[1])
+                scaleTranslationX = midpoint(
+                    initialTouches.touches[0].clientX,
+                    initialTouches.touches[1].clientX
+                )
+                scaleTranslationY = midpoint(
+                    initialTouches.touches[0].clientY,
+                    initialTouches.touches[1].clientY
+                )
 
-            // translationX = translationX * IM[0][0] + IM[0][2]
-            // translationY = translationY * IM[0][0] + IM[1][2]
+                scaleTranslationX = scaleTranslationX * IM[0][0] + IM[0][2]
+                scaleTranslationY = scaleTranslationY * IM[0][0] + IM[1][2]
 
-            let T1: number[][] = [
-                [1, 0, translationX],
-                [0, 1, translationY],
-                [0, 0, 1],
-            ]
-            // let S1: number[][] = [
-            // [controls.view.scale, 0, 0],
-            // [0, controls.view.scale, 0],
-            // [0, 0, 1],
-            // ]
-            // let T2: number[][] = [
-            // [1, 0, -translationX],
-            // [0, 1, -translationY],
-            // [0, 0, 1],
-            // ]
+                let T1: number[][] = [
+                    [1, 0, scaleTranslationX],
+                    [0, 1, scaleTranslationY],
+                    [0, 0, 1],
+                ]
+                let S1: number[][] = [
+                    [controls.view.scale, 0, 0],
+                    [0, controls.view.scale, 0],
+                    [0, 0, 1],
+                ]
+                let T2: number[][] = [
+                    [1, 0, -scaleTranslationX],
+                    [0, 1, -scaleTranslationY],
+                    [0, 0, 1],
+                ]
 
-            let M1: number[][] = multiplyMatrices(M0, T1)
-            // let M2: number[][] = multiplyMatrices(M1, S1)
-            // let MX: number[][] = multiplyMatrices(M2, T2)
+                let M1: number[][] = multiplyMatrices(M0, T1)
+                let M2: number[][] = multiplyMatrices(M1, S1)
+                let MX: number[][] = multiplyMatrices(M3, T3)
 
-            // console.log(`T1:
-            //     ${T1[0]}
-            //     ${T1[1]}
-            //     ${T1[2]}`)
-            // console.log(`S1:
-            //     ${S1[0]}
-            //     ${S1[1]}
-            //     ${S1[2]}`)
-            // console.log(`T2:
-            //     ${T2[0]}
-            //     ${T2[1]}
-            //     ${T2[2]}`)
+                console.log(`T1:
+                ${T1[0]}
+                ${T1[1]}
+                ${T1[2]}`)
+                console.log(`S1:
+                ${S1[0]}
+                ${S1[1]}
+                ${S1[2]}`)
+                console.log(`T2:
+                ${T2[0]}
+                ${T2[1]}
+                ${T2[2]}`)
 
-            // console.log(`M1:
-            //     ${M1[0]}
-            //     ${M1[1]}
-            //     ${M1[2]}`)
-            // console.log(`M2:
-            //     ${M2[0]}
-            //     ${M2[1]}
-            //     ${M2[2]}`)
-            // console.log(`MX:
-            //     ${MX[0]}
-            //     ${MX[1]}
-            //     ${MX[2]}`)
+                console.log(`M1:
+                ${M1[0]}
+                ${M1[1]}
+                ${M1[2]}`)
+                console.log(`M2:
+                ${M2[0]}
+                ${M2[1]}
+                ${M2[2]}`)
+                console.log(`M3:
+                ${M3[0]}
+                ${M3[1]}
+                ${M3[2]}`)
+                console.log(`MX:
+                ${MX[0]}
+                ${MX[1]}
+                ${MX[2]}`)
 
-            // let MXprint: number[][] = MX.map((row) =>
-            //     row.map((number) => Math.round(number * 10) / 10)
-            // )
+                let MXprint: number[][] = MX.map((row) =>
+                    row.map((number) => Math.round(number * 10) / 10)
+                )
 
-            // console.log(`MX:
-            //     ${MXprint[0]}
-            //     ${MXprint[1]}
-            //     ${MXprint[2]}`)
+                console.log(`MX:
+                ${MXprint[0]}
+                ${MXprint[1]}
+                ${MXprint[2]}`)
 
-            controls.view.affineTranslation = M1
+                controls.view.affineTranslation = MX
 
-            return
-        }
+                return
+            }
 
-        function touchEnded(e: TouchEvent) {}
+            function touchEnded(e: TouchEvent) {}
 
-        function resetTouches(e: TouchEvent) {
-            if (!e.touches) return
-            totalInitialTouches = e.touches.length
-            initialTouches = e.touches
-            M0 = controls.view.affineTranslation
-        }
+            function midpoint(point0: number, point1: number) {
+                // Midpoint of two point on the same axis.
+                return (point0 + point1) / 2
+            }
 
-        function midpoint(touches: TouchList) {
-            let [t0, t1] = touches
-            return [
-                (t0.clientX + t1.clientX) / 2,
-                (t0.clientY + t1.clientY) / 2,
-            ]
-        }
+            function distance(point0: number[], point1: number[]) {
+                // Distance between two points on the (x,y) plane.
+                let dx = point0[0] - point1[0]
+                let dy = point0[1] - point1[1]
+                return Math.sqrt(dx * dx + dy * dy)
+            }
 
-        function distance(touches: TouchList) {
-            let [t0, t1] = touches
-            let dx = t0.clientX - t1.clientX
-            let dy = t0.clientY - t1.clientY
-            return Math.sqrt(dx * dx + dy * dy)
-        }
+            //
+            //  Matrix Functions
+            //
 
-        //
-        //  Matrix Functions
-        //
-
-        function multiplyMatrices(m1: number[][], m2: number[][]) {
-            var result: number[][] = []
-            for (var i = 0; i < m1.length; i++) {
-                result[i] = []
-                for (var j = 0; j < m2[0].length; j++) {
-                    var sum = 0
-                    for (var k = 0; k < m1[0].length; k++) {
-                        sum += m1[i][k] * m2[k][j]
+            function multiplyMatrices(m1: number[][], m2: number[][]) {
+                var result: number[][] = []
+                for (var i = 0; i < m1.length; i++) {
+                    result[i] = []
+                    for (var j = 0; j < m2[0].length; j++) {
+                        var sum = 0
+                        for (var k = 0; k < m1[0].length; k++) {
+                            sum += m1[i][k] * m2[k][j]
+                        }
+                        result[i][j] = sum
                     }
-                    result[i][j] = sum
                 }
+                return result
             }
-            return result
-        }
 
-        function inverseAffineMatrix(M: number[][]): number[][] | null {
-            if (!M || M.length !== 3 || M[0].length !== 3) {
-                debug(
-                    `controls.tsx inverseAffineMatrix: Matrix null or wrong dimensions`
-                )
-                return null
-            }
-            if (M[2][0] !== 0 || M[2][1] !== 0 || M[2][2] !== 1) {
-                debug(
-                    `controls.tsx inverseAffineMatrix: Affine matrix bottom row not |0,0,1|`
-                )
-                return null
-            }
-            if (M[0][0] !== M[1][1]) {
-                debug(
-                    `controls.tsx inverseAffineMatrix: Affine matrix scales not equal, M[0][0]!==M[1][1]`
-                )
-                return null
-            }
-            let scale: number = 1 / M[0][0]
-            let tranlateX: number = (-1 * M[0][2]) / M[0][0]
-            let translateY: number = (-1 * M[1][2]) / M[0][0]
+            function inverseAffineMatrix(M: number[][]): number[][] | null {
+                if (!M || M.length !== 3 || M[0].length !== 3) {
+                    debug(
+                        `controls.tsx inverseAffineMatrix: Matrix null or wrong dimensions`
+                    )
+                    return null
+                }
+                if (M[2][0] !== 0 || M[2][1] !== 0 || M[2][2] !== 1) {
+                    debug(
+                        `controls.tsx inverseAffineMatrix: Affine matrix bottom row not |0,0,1|`
+                    )
+                    return null
+                }
+                if (M[0][0] !== M[1][1]) {
+                    debug(
+                        `controls.tsx inverseAffineMatrix: Affine matrix scales not equal, M[0][0]!==M[1][1]`
+                    )
+                    return null
+                }
+                let scale: number = 1 / M[0][0]
+                let tranlateX: number = (-1 * M[0][2]) / M[0][0]
+                let translateY: number = (-1 * M[1][2]) / M[0][0]
 
-            return [
-                [scale, 0, tranlateX],
-                [0, scale, translateY],
-                [0, 0, 1],
-            ]
-        }
+                return [
+                    [scale, 0, tranlateX],
+                    [0, scale, translateY],
+                    [0, 0, 1],
+                ]
+            }
 
-        return {
-            // mousePressed,
-            // mouseDragged,
-            // mouseReleased,
-            touchStarted,
-            touchMoved,
-            touchEnded,
+            return {
+                // mousePressed,
+                // mouseDragged,
+                // mouseReleased,
+                touchStarted,
+                touchMoved,
+                touchEnded,
+            }
         }
     }
 }
